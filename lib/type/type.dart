ca2e520d0edfd7a2b316e79204be3f885915efb1
‎ <>
2025-01-05 20:35:57 +0700

    up

--- lib/type/type.dart
+++ lib/type/type.dart
@@ -1,10 +1,65 @@
 enum GeneralLibClientType {
-  bot,
-  userbot,
-  normal,
+  unknown(title: "All"),
+  bot(title: "Bot"),
+  userbot(title: "Userbot"),
+  normal(title: "Normal");
+
+  final String title;
+  const GeneralLibClientType({
+    required this.title,
+  });
+
+  static GeneralLibClientType getFromText({
+    required String text,
+  }) {
+    for (final element in values) {
+      if (element.name == text) {
+        return element;
+      }
+    }
+    return GeneralLibClientType.unknown;
+  }
+
+  static GeneralLibClientType getFromTextOrDefault({
+    required String text,
+    required GeneralLibClientType Function() onDefault,
+  }) {
+    final GeneralLibClientType generalLibClientType = getFromText(text: text);
+    if (generalLibClientType == GeneralLibClientType.unknown) {
+      return onDefault();
+    }
+    return generalLibClientType;
+  }
 }
 
 enum GeneralLibClientUseType {
-  paid,
-  free,
+  unknown(title: "All"),
+  paid(title: "Paid"),
+  free(title: "Free");
+
+  final String title;
+  const GeneralLibClientUseType({
+    required this.title,
+  });
+  static GeneralLibClientUseType getFromText({
+    required String text,
+  }) {
+    for (final element in values) {
+      if (element.name == text) {
+        return element;
+      }
+    }
+    return GeneralLibClientUseType.unknown;
+  }
+
+  static GeneralLibClientUseType getFromTextOrDefault({
+    required String text,
+    required GeneralLibClientUseType Function() onDefault,
+  }) {
+    final GeneralLibClientUseType generalLibClientUseType = getFromText(text: text);
+    if (generalLibClientUseType == GeneralLibClientUseType.unknown) {
+      return onDefault();
+    }
+    return generalLibClientUseType;
+  }
 }
