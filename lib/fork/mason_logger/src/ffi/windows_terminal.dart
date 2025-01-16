5ffce353b9581de98374bba38ecd04c97df2578e
‎ <>
2025-01-16 21:00:40 +0700

    up

---
+++ lib/fork/mason_logger/src/ffi/windows_terminal.dart
@@ -0,0 +1,27 @@
+// coverage:ignore-file
+// ignore_for_file: public_member_api_docs
+
+import 'package:general_lib/fork/mason_logger/src/ffi/terminal.dart';
+import 'package:win32/win32.dart';
+
+class WindowsTerminal implements Terminal {
+  WindowsTerminal() {
+    outputHandle = GetStdHandle(STD_HANDLE.STD_OUTPUT_HANDLE);
+    inputHandle = GetStdHandle(STD_HANDLE.STD_INPUT_HANDLE);
+  }
+
+  late final int inputHandle;
+  late final int outputHandle;
+
+  @override
+  void enableRawMode() {
+    const dwMode = (~CONSOLE_MODE.ENABLE_ECHO_INPUT) & (~CONSOLE_MODE.ENABLE_PROCESSED_INPUT) & (~CONSOLE_MODE.ENABLE_LINE_INPUT) & (~CONSOLE_MODE.ENABLE_WINDOW_INPUT);
+    SetConsoleMode(inputHandle, dwMode);
+  }
+
+  @override
+  void disableRawMode() {
+    const dwMode = CONSOLE_MODE.ENABLE_ECHO_INPUT | CONSOLE_MODE.ENABLE_EXTENDED_FLAGS | CONSOLE_MODE.ENABLE_INSERT_MODE | CONSOLE_MODE.ENABLE_LINE_INPUT | CONSOLE_MODE.ENABLE_MOUSE_INPUT | CONSOLE_MODE.ENABLE_PROCESSED_INPUT | CONSOLE_MODE.ENABLE_QUICK_EDIT_MODE | CONSOLE_MODE.ENABLE_VIRTUAL_TERMINAL_INPUT;
+    SetConsoleMode(inputHandle, dwMode);
+  }
+}
---
+++ lib/fork/mason_logger/src/link.dart
@@ -0,0 +1,23 @@
+import 'package:io/ansi.dart';
+
+/// Wraps [uri] with an escape sequence so it's recognized as a hyperlink.
+/// An optional message can be used in place of the [uri].
+/// If no [message] is provided, the text content will be the full [uri].
+///
+/// ```dart
+/// final plainLink = link(uri: Uri.parse('https://dart.dev'));
+/// print(plainLink); // Equivalent to `[https://dart.dev](https://dart.dev)` in markdown
+///
+/// final richLink = link(uri: Uri.parse('https://dart.dev'), message: 'The Dart Website');
+/// print(richLink); // Equivalent to `[The Dart Website](https://dart.dev)` in markdown
+/// ```
+String link({required Uri uri, String? message}) {
+  if (!ansiOutputEnabled) {
+    return message != null ? '[$message]($uri)' : uri.toString();
+  }
+
+  const leading = '\x1B]8;;';
+  const trailing = '\x1B\\';
+
+  return '$leading$uri$trailing${message ?? uri}$leading$trailing';
+}
